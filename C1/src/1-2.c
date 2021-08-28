20823aaacb1c5e8f255fd92a7331c251a669ea95
Wetter <863688821@qq.com>
2021-08-28 09:53:02 +0800

    Create 1-2.c

---
+++ C1/src/1-2.c
@@ -0,0 +1,14 @@
+/*
+Exercise 1-2. 
+Experiment to find out what happens when prints's argument string contains
+\c, where c is some character not listed above.
+*/
+
+#include <stdio.h>
+
+int main (int argc, char const *argv[])
+{
+    printf("Hello world!\c\n");
+}
+
+// warning: unknown escape sequence: '\c'
